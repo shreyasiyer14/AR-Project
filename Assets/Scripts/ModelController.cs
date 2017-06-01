28bdc811cda075d5cb45ebdf8747b7e587b8abbe
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-06-01 15:44:09 +0530

    Added basic model controller for changing size, playing animations and made the project Vuforia based.

---
+++ Assets/Scripts/ModelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ModelController : MonoBehaviour {
+	[SerializeField] private string idleAnim;
+	[SerializeField] private string walkAnim;
+	[SerializeField] private float speed;
+	[SerializeField] private int directionOfWalk;
+	[SerializeField] private Slider scaleSlider;
+	private float scale;
+	private bool isWalking = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isWalking) {
+			GetComponent<Animation> ().wrapMode = WrapMode.Loop;
+			GetComponent<Animation> ().CrossFade (idleAnim);
+		} else {
+			GetComponent<Animation> ().CrossFade (walkAnim);
+			transform.Translate (directionOfWalk * Vector3.forward * speed * Time.fixedDeltaTime);
+		}
+	}
+
+	public void modifyScale () {
+		transform.localScale = new Vector3 (scaleSlider.value, scaleSlider.value, scaleSlider.value);
+	}
+
+	public void walkAnimStart () {
+		isWalking = true;
+	}
+}
