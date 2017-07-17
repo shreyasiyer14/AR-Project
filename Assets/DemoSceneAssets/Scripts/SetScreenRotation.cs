945951e57aba846185491e581b453b339eac4eb4
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-07-17 22:25:18 +0530

    Added sensor scripts

---
+++ Assets/DemoSceneAssets/Scripts/SetScreenRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetScreenRotation : MonoBehaviour {
+
+	public ScreenOrientation so = ScreenOrientation.Landscape;
+
+	// Use this for initialization
+	void Awake () {
+		Screen.orientation = so;
+	}
+}
