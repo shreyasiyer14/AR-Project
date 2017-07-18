7d5605a89e2619a3ce75ab3322c4a85b7518744c
Avi K <kushagrakhare@rocketmail.com>
2017-07-18 22:10:28 +0530

    Added feature of automatically adjusting the brightness of the spider with the help of light sensors.

---
+++ Assets/Scripts/LightsensorValue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LightsensorValue : MonoBehaviour
+{
+
+    Text txt;
+    private int currentscore = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        txt = gameObject.GetComponent<Text>();
+        txt.text = Sensor.light + "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = Sensor.light + "";
+    }
+}
