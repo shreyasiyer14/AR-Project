60c20e0fd580dc9273d3720bc1a1cddd21fa0fda
Avi K <kushagrakhare@rocketmail.com>
2017-07-18 22:10:28 +0530

    Added feature of automatically adjusting the brightness of the spider with the help of light sensors.

--- Assets/GyroDroid/SampleScripts/LightControl.cs
+++ Assets/GyroDroid/SampleScripts/LightControl.cs
@@ -11,7 +11,7 @@ public class LightControl : MonoBehaviour {
 	public Color dayBackgroundColor;
 	public Color nightBackgroundColor;
 	public Color darkAmbient = new Color(0.1f,0.1f,0.1f);
-	public Color lightAmbient = new Color(0.7f,0.7f,0.7f);
+	public Color lightAmbient = new Color(1.5f,1.5f,1.5f);
 	
 	// Use this for initialization
 	void Start () {
@@ -23,22 +23,29 @@ public class LightControl : MonoBehaviour {
 	void Update () {
 		// fetch light sensor
 		float lightValue = Sensor.light;
-		RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, new Color(lightValue/100f, lightValue/100f, lightValue/100f), Time.deltaTime);
-
-//		// compare to predefined LightValue constants
-//		if(lightValue < Sensor.LightValue.Cloudy)
-//			ItIsNight(true);
-//		else
-//			ItIsNight(false);
-	}
-//	
-//	void ItIsNight(bool on) {
-//		// slowly fade colors and ambientLight settings
-//		if(on) {
-//			RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, darkAmbient, Time.deltaTime);
-//		}
-//		else {
-//			RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, lightAmbient, Time.deltaTime);
-//		}		
-//	}
+        float maxWhite = 1.0f;
+        int maxLightValue = 150;
+        float divideFactor = maxLightValue / maxWhite;
+        if(lightValue!=0)
+		    RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, new Color(lightValue/divideFactor, lightValue/divideFactor, lightValue/divideFactor), Time.deltaTime);
+        else
+            RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, new Color(0.001f,0.001f,0.001f), Time.deltaTime);
+        //RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, new Color(lightValue/15f, lightValue / 15f, lightValue / 15f), Time.deltaTime);
+        //Debug.Log(lightValue + "\n");
+        //		// compare to predefined LightValue constants
+        //		if(lightValue < Sensor.LightValue.Cloudy)
+        //			ItIsNight(true);
+        //		else
+        //			ItIsNight(false);
+    }
+    //	
+    	//void ItIsNight(bool on) {
+    		// slowly fade colors and ambientLight settings
+    		//if(on) {
+    		//	RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, darkAmbient, Time.deltaTime);
+    		//}
+    		//else {
+    		//	RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, lightAmbient, Time.deltaTime);
+    		//}		
+    	//}
 }
