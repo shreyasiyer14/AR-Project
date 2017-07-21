574e8870d3bdbfb241a165810d9ee2ea6eacd165
Avi K <kushagrakhare@rocketmail.com>
2017-07-21 23:36:07 +0530

    Added on/off button to switch models on and off.

---
+++ Assets/Scripts/ImageTargetDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetDetector : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public Transform emptyTrans;
+    public GameObject currentsModels;
+    private GameObject target;
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        target = gameObject;
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            currentsModels.transform.SetParent(target.transform);
+            currentsModels.transform.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Switch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+    [SerializeField] private Button switchButton;
+    public bool isOn = false;
+	
+    public void switchTrigger ()
+    {
+        if (!isOn)
+        {
+            isOn = true;
+            switchButton.transform.GetChild(0).gameObject.GetComponent<Text>().text = "OFF";
+        }
+        else if (isOn)
+        {
+            isOn = false;
+            switchButton.transform.GetChild(0).gameObject.GetComponent<Text>().text = "ON";
+        }
+        gameObject.SetActive(isOn);
+    }
+}
